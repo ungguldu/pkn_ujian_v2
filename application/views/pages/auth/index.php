0310649b93c529daff55899bd7cbe8610e1ab954
Faris Zain <farisz@pknstan.ac.id>
2022-11-05 23:37:52 +0700

    templating auth

--- application/views/pages/auth/index.php
+++ application/views/pages/auth/index.php
@@ -1,20 +1,8 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 
-<div class="card">
-    <div class="empty">
-        <div class="empty-img mb-0" style="min-height: 256px;">
-            <img src="<?= base_url('assets/img/exam_welcome.png'); ?>" class="h-100" height="360" alt="Welcome to Exam">
-        </div>
-        <p class="empty-title">Selamat Datang Peserta Ujian</p>
-        <p class="empty-subtitle text-muted">
-            Kesungguhan, kejujuran tidak akan menghianati hasil. <br>
-            Do your best bro .. 👍
-        </p>
-        <div class="empty-action mb-4">
-            <a href="<?= site_url('auth/mahasiswa'); ?>" class="btn btn-primary">
-                <i class="icon ti ti-login"></i>
-                Login Sekarang
-            </a>
-        </div>
+<div>
+    <div class="text-center">
+        <img src="<?= base_url('assets/img/exam_paper.png') ?>" alt="welcome" height="256" onclick="window.location.replace('<?= site_url('auth/pengawas'); ?>')">
+        <a href="<?= site_url('auth/mahasiswa'); ?>" class="btn btn-primary mt-3 w-100">Mulai Sekarang ..</a>
     </div>
 </div>
--- application/views/template_auth.php
+++ application/views/template_auth.php
@@ -12,27 +12,35 @@
     <title>Portal Ujian - PKN STAN</title>
     <link rel="shortcut icon" href="<?= base_url('assets/favicon.ico'); ?>" type="image/x-icon">
     <!-- CSS files -->
-    <link href="<?= base_url('assets/'); ?>css/tabler.min.css" rel="stylesheet" />
+    <link href="<?= base_url('assets/css/tabler.min.css'); ?>" rel="stylesheet" />
     <link href="<?= base_url('assets/tabler-icons.min.css'); ?>" rel="stylesheet">
 </head>
 
-<body class=" border-top-wide border-primary d-flex flex-column">
-    <div class="page page-center">
-        <div class="container-tight py-3">
-            <div class="text-center mb-5">
-                <a href="<?= site_url('/'); ?>" class="navbar-brand navbar-brand-autodark"><img src="<?= base_url('assets/img/logo_exam.svg') ?>" height="72" alt="Logo Portal Ujian"></a>
-            </div>
-            <!-- form -->
-
-            <?php $this->load->view(!empty($page) ? $page : 'pages/auth/index', false); ?>
-            <!-- footer -->
-            <div class="text-center text-muted mt-3">
-                <small class="text-muted">Kolaborasi Unit Sistem Informasi dan Bagian Akademik PKN STAN</small>
+<body class="d-flex flex-column">
+    <div class="row g-0 flex-fill">
+        <div class="col-12 col-lg-6 col-xl-5 border-top-wide border-primary d-flex flex-column justify-content-center">
+            <div class="container container-tight my-3 px-lg-3">
+                <div class="text-center mb-4">
+                    <a href="." class="navbar-brand navbar-brand-autodark">
+                        <img src="<?= base_url('assets/img/logo_exam.svg') ?>" alt="logo" height="42">
+                    </a>
+                </div>
+                <?php
+                    $page = $page ?? 'pages/auth/index';
+                    $this->load->view($page, null, FALSE);
+                ?>
+                <div class="text-center text-muted mt-3">
+                    <small>Subbagian Akademik PKN STAN &copy; <?= date('Y'); ?></small>
+                </div>
             </div>
         </div>
+        <div class="col-12 col-lg-6 col-xl-7 d-none d-lg-block">
+            <div class="bg-cover h-100 min-vh-100" style="background-image: url('<?= base_url('assets/img/world_map.svg') ?>')"></div>
+        </div>
     </div>
+    <!-- Libs JS -->
     <!-- Tabler Core -->
-    <script src="<?= base_url('assets/'); ?>js/tabler.min.js" defer></script>
+    <script src="<?= base_url('assets/js/tabler.min.js') ?>" defer=""></script>
 </body>
 
 </html>
