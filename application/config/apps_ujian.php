52947ad9309804a01045c15ffe64aed93a541248
Faris Zain <farisz@pknstan.ac.id>
2022-11-05 23:59:00 +0700

    tambah config apps

---
+++ application/config/apps_ujian.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$config['nama_aplikasi'] = 'Box Ujian - PKN STAN';
+/**
+ * config base directory masing topik.
+ */
+$config['dir_template'] = WRITEPATH.'templates'.DIRECTORY_SEPARATOR;
+$config['dir_soal'] = WRITEPATH . 'masalah' . DIRECTORY_SEPARATOR;
+$config['dir_jawaban'] = WRITEPATH . 'jawaban' . DIRECTORY_SEPARATOR;
+$config['dir_zip'] = WRITEPATH . 'zip' . DIRECTORY_SEPARATOR;
+$config['dir_BA'] = WRITEPATH . 'ba_ujian' . DIRECTORY_SEPARATOR;
