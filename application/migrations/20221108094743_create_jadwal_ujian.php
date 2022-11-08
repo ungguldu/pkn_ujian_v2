8a19a2458bd28cb1dd0fcb48addc7011cbc5cef5
Faris Zain <farisz@pknstan.ac.id>
2022-11-08 10:04:49 +0700

    update jadwal tambah durasi

--- application/migrations/20221108094743_create_jadwal_ujian.php
+++ application/migrations/20221108094743_create_jadwal_ujian.php
@@ -21,21 +21,31 @@ class Migration_create_jadwal_ujian extends CI_Migration
             ),
             'tanggal' => array(
                 'type' => 'DATE',
+                'null' => TRUE,
             ),
             'waktu_mulai' => array(
                 'type' => 'TIME',
+                'null' => TRUE,
             ),
             'sesi' => array(
                 'type' => 'INT',
-                'constraint' => '3',
+                'constraint' => '2',
+                'null' => TRUE,
             ),
             'program_studi' => array(
                 'type' => 'VARCHAR',
                 'constraint' => '255',
+                'null' => TRUE,
             ),
             'mata_kuliah' => array(
                 'type' => 'VARCHAR',
                 'constraint' => '255',
+                'null' => TRUE,
+            ),
+            'durasi_pengerjaan' => array(
+                'type' => 'INT',
+                'constraint' => '3',
+                'null' => TRUE,
             ),
         ));
 
