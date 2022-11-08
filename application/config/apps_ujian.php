a5eca7c48ddcd5db433027fedc4a4d140a814d82
Faris Zain <farisz@pknstan.ac.id>
2022-11-08 12:21:04 +0700

    update durasi

--- application/config/apps_ujian.php
+++ application/config/apps_ujian.php
@@ -6,8 +6,16 @@ $config['nama_aplikasi'] = 'Box Ujian - PKN STAN';
 /**
  * config base directory masing topik.
  */
-$config['dir_template'] = WRITEPATH.'templates'.DIRECTORY_SEPARATOR;
+$config['dir_data'] = WRITEPATH.'data'.DIRECTORY_SEPARATOR;
+$config['dir_template'] = WRITEPATH . 'templates' . DIRECTORY_SEPARATOR;
 $config['dir_soal'] = WRITEPATH . 'masalah' . DIRECTORY_SEPARATOR;
 $config['dir_jawaban'] = WRITEPATH . 'jawaban' . DIRECTORY_SEPARATOR;
 $config['dir_zip'] = WRITEPATH . 'zip' . DIRECTORY_SEPARATOR;
 $config['dir_BA'] = WRITEPATH . 'ba_ujian' . DIRECTORY_SEPARATOR;
+
+/**
+ * durasi pengerjaan
+ */
+$config['durasi_3sks'] = 180; // dalam menit
+$config['durasi_2sks'] = 120; // dalam menit
+$config['durasi_1sks'] = 120; // dalam menit
