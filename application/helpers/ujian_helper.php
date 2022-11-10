c51d02ffc30c30b219ba74947443f16af6d7e849
Faris Zain <farisz@pknstan.ac.id>
2022-11-10 15:27:42 +0700

    tambah adjust mulai jadwal

--- application/helpers/ujian_helper.php
+++ application/helpers/ujian_helper.php
@@ -1,12 +1,12 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-function izinkan_ujian(object|array $jadwal = null)
+function izinkan_ujian(object|array $jadwal = null, int $adjust_waktu_mulai = 0)
 {
     if(empty($jadwal)) return false;
     $CI = &get_instance();
 
-    $sesi = $jadwal->sesi;
-    switch ($sesi) {
+    $sks = $jadwal->sks;
+    switch ($sks) {
         case '3':
             $pengerjaan = $CI->config->item('durasi_3sks');
             break;
@@ -21,9 +21,10 @@ function izinkan_ujian(object|array $jadwal = null)
             break;
     }
 
-    $durasi_ujian = !empty($jadwal->durasi_pengerjaan) ? $jadwal->durasi_pengerjaan + $CI->config->item('durasi_pengumpulan') : $CI->config->item('durasi_pengumpulan') + $pengerjaan; // dalam menit
-    $waktu_akses = date('Y-m-d H:i:s');
-    $waktu_ujian = $jadwal->tanggal . ' ' . $jadwal->waktu_mulai;
+    $tambahan = $CI->config->item('durasi_pengumpulan');
+    $durasi_ujian = !empty($jadwal->durasi_pengerjaan) ? $jadwal->durasi_pengerjaan + $tambahan : $pengerjaan + $tambahan; // dalam menit
+    $waktu_akses = !empty($adjust_waktu_mulai) ? date('Y-m-d H:i:s', strtotime(date('Y-m-d H:i:s') . ' ' . $durasi_ujian . ' minutes')) : date('Y-m-d H:i:s');
+    $waktu_ujian =  $jadwal->tanggal . ' ' . $jadwal->waktu_mulai;
     $sesi_max = date('Y-m-d H:i:s', strtotime($waktu_ujian . ' + '.$durasi_ujian.' minutes'));
     $izinkan = ($waktu_akses > $waktu_ujian and $waktu_akses < $sesi_max) ? true : false;
     $sisa_sesi = strtotime($sesi_max) - strtotime($waktu_akses);
