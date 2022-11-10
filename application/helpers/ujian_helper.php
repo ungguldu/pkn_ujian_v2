80a41d026fe1ee0d6dda15098fe8f9b86703983c
Faris Zain <farisz@pknstan.ac.id>
2022-11-10 16:30:43 +0700

    koreksi logic dan dokumentasi

--- application/helpers/ujian_helper.php
+++ application/helpers/ujian_helper.php
@@ -20,15 +20,19 @@ function izinkan_ujian(object|array $jadwal = null, int $adjust_waktu_mulai = 0)
             $pengerjaan = 0;
             break;
     }
-
     $tambahan = $CI->config->item('durasi_pengumpulan');
+    // ubah ke base integer strtotime
+    $waktu_ujian_ori = strtotime($jadwal->tanggal . ' ' . $jadwal->waktu_mulai); // y-m-d
+    $waktu_ujian = !empty($adjust_waktu_mulai) ? strtotime($jadwal->tanggal . ' ' . $jadwal->waktu_mulai .' '.$adjust_waktu_mulai .' minutes') : $waktu_ujian_ori;
+    // durasi dalam menit
     $durasi_ujian = !empty($jadwal->durasi_pengerjaan) ? $jadwal->durasi_pengerjaan + $tambahan : $pengerjaan + $tambahan; // dalam menit
-    $waktu_akses = !empty($adjust_waktu_mulai) ? date('Y-m-d H:i:s', strtotime(date('Y-m-d H:i:s') . ' ' . $durasi_ujian . ' minutes')) : date('Y-m-d H:i:s');
-    $waktu_ujian =  $jadwal->tanggal . ' ' . $jadwal->waktu_mulai;
-    $sesi_max = date('Y-m-d H:i:s', strtotime($waktu_ujian . ' + '.$durasi_ujian.' minutes'));
+    // waktu akses base int
+    $waktu_akses = strtotime(date('Y-m-d H:i:s'));
+    // sesi max int
+    $sesi_max = strtotime(date('Y-m-d H:i:s', $waktu_ujian_ori) . ' + '.$durasi_ujian.' minutes');
     $izinkan = ($waktu_akses > $waktu_ujian and $waktu_akses < $sesi_max) ? true : false;
-    $sisa_sesi = strtotime($sesi_max) - strtotime($waktu_akses);
-    return ['izinkan' => $izinkan, 'sisa_sesi' => $sisa_sesi, 'durasi_ujian' => $durasi_ujian];
+    $sisa_sesi = $sesi_max - $waktu_akses;
+    return ['izinkan' => $izinkan, 'sisa_sesi' => $sisa_sesi, 'durasi_ujian' => $durasi_ujian, 'akses_terakhir' => date('Y-m-d H:i:s', $sesi_max)];
 }
 
 function nama_file_folder(string $str = null)
